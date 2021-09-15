bc0dd63f8dc920485d83c46d2b99369f40eb668f
ti777 <tifany090295@hotmail.com>
2021-09-15 21:50:19 +0200

    test shouldReturn404StatusCode (green)

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -5,9 +5,34 @@ import java.io.InputStream;
 import java.net.Socket;
 
 public class HttpClient {
-    public HttpClient(String host, int port, String requestTarget) {
+
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        socket.getOutputStream().write(
+                ("GET " + requestTarget + " HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "\r\n").getBytes()
+        );
+
+        StringBuilder result = new StringBuilder();
+        InputStream in = socket.getInputStream();
+        int c;
+        while ((c = in.read()) != -1) {
+            result.append((char)c);
+        }
+        String responseMessage = result.toString();
+        this.statusCode = Integer.parseInt(responseMessage.split(" ")[1]);
 
     }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("httpbin.org", 80);
@@ -26,9 +51,5 @@ public class HttpClient {
             System.out.print((char)c);
         }
 
-    }
-
-    public int getStausCode() {
-        return 200;
     }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,6 +2,8 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
@@ -12,15 +14,15 @@ class HttpClientTest {
     }
 
     @Test
-    void shouldReturnStatusCode() {
+    void shouldReturnStatusCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org", 80, "/html");
-        assertEquals(200, client.getStausCode());
+        assertEquals(200, client.getStatusCode());
     }
 
     @Test
-    void shouldReturn404StatusCode() {
+    void shouldReturn404StatusCode() throws IOException {
         HttpClient client = new HttpClient("Httpbin.org", 80, "/html");
-        assertEquals(404, client.getStausCode());
+        assertEquals(404, client.getStatusCode());
     }
 
 }
